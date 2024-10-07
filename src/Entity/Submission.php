306a4ee363495c6bf8404320a2dbd8d88d619066
monaim <ennabbalimonaim@gmail.com>
2024-10-08 00:00:37 +0200

    add lesson and submission entities

---
+++ src/Entity/Submission.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubmissionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SubmissionRepository::class)
+ */
+class Submission
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $grade;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $feedback;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGrade(): ?int
+    {
+        return $this->grade;
+    }
+
+    public function setGrade(int $grade): self
+    {
+        $this->grade = $grade;
+
+        return $this;
+    }
+
+    public function getFeedback(): ?string
+    {
+        return $this->feedback;
+    }
+
+    public function setFeedback(string $feedback): self
+    {
+        $this->feedback = $feedback;
+
+        return $this;
+    }
+}
